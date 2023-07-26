0bece58fa7c184ff7ac7d0d3697c2dfda11890d0
Alaa Tarek <alaatarek898@gmail.com>
2023-07-26 21:05:41 +0300

    Rating-stars

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Starrating from "./Star";
+import "./index.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Starrating
+        maxrating={5}
+        color="red"
+        size={40}
+        message={["Terrible", "Bad", "Good", "VeryGood", "Amazing"]}
+      />
+      <Starrating maxrating={10} size={50} />
     </div>
   );
 }
